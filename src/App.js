e733297e2a750ed53508b7fc0147833a9ef150c5
abisummers <hello@abisummers.com>
2018-11-22 23:28:59 +0100

    imported counter component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
 
 class App extends Component {
+  state = {
+    colour: "green"
+  };
   render() {
+    console.log(this.state);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hello World</h1>
+
+        <Counter colour={this.state.colour} />
+        <Counter colour={this.state.colour === "red" ? "green" : "red"} />
+        <button
+          onClick={event => {
+            this.setState(state => {
+              return { colour: state.colour === "green" ? "red" : "green" };
+            });
+          }}
+        >
+          Change Colour
+        </button>
       </div>
     );
   }
